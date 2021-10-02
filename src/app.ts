16771ca3ce0821a3d69b6822d0904beab2e39522
Dillon Cutaiar <dillonc@vt.edu>
2021-10-01 19:44:19 -0600

    initial changes to personalize and get started

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,34 @@
 import P5 from "p5";
 import "p5/lib/addons/p5.dom";
-// import "p5/lib/addons/p5.sound";	// Include if needed
-import "./styles.scss";
 
-// DEMO: A sample class implementation
-import MyCircle from "./MyCircle";
+import { Cell, createRandomCells } from "./Cell";
 
-// Creating the sketch itself
-const sketch = (p5: P5) => {
-	// DEMO: Prepare an array of MyCircle instances
-	const myCircles: MyCircle[] = [];
+var sketch = (p: P5) => {
+  let cells: Cell[];
 
-	// The sketch setup method 
-	p5.setup = () => {
-		// Creating and positioning the canvas
-		const canvas = p5.createCanvas(200, 200);
-		canvas.parent("app");
+  p.setup = () => {
+    console.log("🚀 - Setup initialized - P5 is running");
 
-		// Configuring the canvas
-		p5.background("white");
+    p.createCanvas(p.windowWidth, p.windowHeight);
 
-		// DEMO: Create three circles in the center of the canvas
-		for (let i = 1; i < 4; i++) {
-			const p = p5.width / 4;
-			const circlePos = p5.createVector(p * i, p5.height / 2);
-			const size = i % 2 !== 0 ? 24 : 32;
-			myCircles.push(new MyCircle(p5, circlePos, size));
-		}
-	};
+    cells = createRandomCells(p, 100);
+  };
 
-	// The sketch draw method
-	p5.draw = () => {
-		// DEMO: Let the circle instances draw themselves
-		myCircles.forEach(circle => circle.draw());
-	};
+  p.windowResized = () => {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  };
+
+  p.draw = () => {
+    p.background(0);
+
+    cells.forEach((c) => {
+      c.tick();
+    });
+
+    cells.forEach((c) => {
+      c.draw();
+    });
+  };
 };
 
 new P5(sketch);
