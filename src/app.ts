714036947a453cf2f7dbd71aa45457c132509132
cutaiar <dillonc@vt.edu>
2023-04-26 11:58:07 -0600

    remove starter

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,13 @@
 import P5 from "p5";
 import "p5/lib/addons/p5.dom";
 
-import { Cell, createRandomCells } from "./Cell";
-
 var sketch = (p: P5) => {
-  let cells: Cell[];
 
   p.setup = () => {
     console.log("🚀 - Setup initialized - P5 is running");
 
     p.createCanvas(p.windowWidth, p.windowHeight);
 
-    cells = createRandomCells(p, 100);
   };
 
   p.windowResized = () => {
@@ -21,13 +17,6 @@ var sketch = (p: P5) => {
   p.draw = () => {
     p.background(0);
 
-    cells.forEach((c) => {
-      c.tick();
-    });
-
-    cells.forEach((c) => {
-      c.draw();
-    });
   };
 };
 
